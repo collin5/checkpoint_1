59a0fe0c87d0e8953a62d638a37f3e253d0226be
Collins Abitekaniza <abtcolns@gmail.com>
2017-06-09 13:10:40 +0300

    Room class structure

--- modules/room.py
+++ modules/room.py
@@ -1,7 +1,14 @@
 # @Author: collins
 # @Date:   2017-06-09T12:16:10+03:00
 # @Last modified by:   collins
-# @Last modified time: 2017-06-09T12:16:10+03:00
+# @Last modified time: 2017-06-09T13:10:06+03:00
 
 
+class Room(list):
 
+    def __init__(room_name, capacity, people=[]):
+        super(Room, self).__init__()
+        self.room_name, self.capacity, self.people = room_name, capacity, people
+
+    def add_people(*args):
+        pass
