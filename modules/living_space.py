0df1790ee45cbd5aed30432ef535e31d2112f2a8
Collins Abitekaniza <abtcolns@gmail.com>
2017-06-09 14:05:03 +0300

    Office, LivingSpace class structures

--- modules/living_space.py
+++ modules/living_space.py
@@ -1,7 +1,14 @@
 # @Author: collins
 # @Date:   2017-06-09T12:15:22+03:00
 # @Last modified by:   collins
-# @Last modified time: 2017-06-09T12:15:22+03:00
+# @Last modified time: 2017-06-09T14:04:31+03:00
+
+from room import Room
+
+MAX_PERSONS = 4  # Living space only allows a maximum of 4 people
 
 
+class LivingSpace(Room):
 
+    def __init__(self, room_name, people):
+        super(LivingSpace, self).__init__(room_name, MAX_PERSONS, people)
--- modules/office.py
+++ modules/office.py
@@ -1,7 +1,15 @@
 # @Author: collins
 # @Date:   2017-06-09T12:15:41+03:00
 # @Last modified by:   collins
-# @Last modified time: 2017-06-09T12:15:41+03:00
+# @Last modified time: 2017-06-09T14:01:20+03:00
 
 
+from room import Room
 
+MAX_PERSONS = 6  # office only allows maximum of 6 people
+
+
+class Office(Room):
+
+    def __init__(self, room_name, people=[]):
+        super(Office, self).__init__(room_name, MAX_PERSONS, people)
